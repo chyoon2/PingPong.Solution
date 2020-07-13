a75fbb723dc94e7e12cdc4215485e5cb19ffcfb9
Chris Yoon <chy.yoon@gmail.com>
2020-07-13 14:16:19 -0700

    add a list and for loop to print out numbered list
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

--- Program2.cs
+++ Program2.cs
@@ -12,3 +12,13 @@ public class Program
     Console.WriteLine("Enter a number:");
     string stringUserNumber = Console.ReadLine();
     int userNumber = int.Parse(stringUserNumber);
+    PingNumber userPingNumber = new PingNumber(userNumber);
+    List<string> userList = new List<string> {};
+    userList = userPingNumber.GetNumberList();
+    for (int i = 0; i < userList.Count; i++)
+    {
+      Console.WriteLine(userList[i]);
+    }
+  }
+}
+}
