86b21a363938a383131d3b6be12de5a91229306e
Chris Yoon <chy.yoon@gmail.com>
2020-07-13 13:27:38 -0700

    add a new prgram.cs file
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

---
+++ Program2.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using Game.Models;
+
+namespace Game
+{
+public class Program
+{
+  public static void Main()
+  {
+    
+    Console.WriteLine("Enter a number:");
+    string stringUserNumber = Console.ReadLine();
+    int userNumber = int.Parse(stringUserNumber);
